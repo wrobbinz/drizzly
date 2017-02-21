a60a393a0cb2b7cb7e068f53bc732505ad1d6a4b
wrobbinz <wrobbinz@gmail.com>
2017-02-21 00:05:40 -0800

    various fixes

--- src/api/news/index.js
+++ src/api/news/index.js
@@ -10,9 +10,8 @@ import _ 				from 'underscore'
 const apiKey		= "cf2cec0cee9544839c4ade13a131f33a"
 const newsApiSource	= ["ars-technica", "associated-press", "bbc-news", "bbc-sport", "bloomberg", "business-insider", "cnbc", "cnn", "engadget", "entertainment-weekly", "espn", "financial-times", "google-news", "hacker-news", "national-geographic", "new-scientist", "newsweek", "reddit-r-all", "reuters", "techcrunch", "the-economist", "the-guardian-uk", "the-huffington-post", "the-new-york-times", "the-wall-street-journal", "the-washington-post", "time", "usa-today"] 
 const newsSection	= ["business", "entertainment", "all", "politics", "sports", "technology"]
-const filteredWord	= ["has", "been", "into", "had", "not", "this", "was", "its", "their", "so", "after", "with", "over", "what", "from", "by", "an", "could", "how", "i",  "it", "be", "if", "in", "then", "than", "a", "but", "or", "and", "why", "on", "out", "is", "at", "to", "the", "are", "for", "of", "as"]
+const filteredWord	= ["his", "her", "between", "just", "when", "too", "also", "they", "that", "&", "has", "been", "into", "had", "not", "this", "was", "its", "their", "so", "after", "with", "over", "what", "from", "by", "an", "could", "how", "i",  "it", "be", "if", "in", "then", "than", "a", "but", "or", "and", "why", "on", "out", "is", "at", "to", "the", "are", "for", "of", "as"]
 let rank			= "top"
-let sourceString 		= ""
 
 
 // router
@@ -26,9 +25,9 @@ export default ({ config }) => {
       // To: ['source1article1', 'source1article2', 'source2article1', ...]
       const articles = [].concat.apply([], apiResponses)
       // Pass the articles as parameter
-      const theWords = processWordBank(articles)
+      const wordCloud = processWordBank(articles)
       // Respond with the processed object
-      res.json( theWords )
+      res.json( wordCloud )
     })
 	})
 
--- src/db.js
+++ src/db.js
@@ -1,4 +1,9 @@
 export default callback => {
 	// connect to a database if needed, then pass it to `callback`:
-	callback();
+	const MongoClient = require('mongodb').MongoClient
+	
+	MongoClient.connect('mongodb://admin:RainbowRoadNewspoint@ds157549.mlab.com:57549/newspoint', (err, database) => {
+		if (err) return console.log(err)
+	})
+	callback()
 }
