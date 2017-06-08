aef67dd4ee5b4e1a916b04d6d19941d0d2fb869f
wrobbinz <wrobbinz@gmail.com>
2017-06-07 23:30:35 -0700

    latest?

--- src/api/news/index.js
+++ src/api/news/index.js
@@ -1,10 +1,8 @@
-import { Router } 		                  from 'express'
-import https 			                      from 'https'
-import bodyParser 		                  from 'body-parser'
-import rp 				                      from 'request-promise'
-import _                                from 'lodash'
-import { newsApiSource, filteredWord }  from './modules/sources'
-import db                               from '../../queries'
+import { Router } from 'express'
+import rp from 'request-promise'
+import _ from 'lodash'
+import { newsApiSource, filteredWord } from './modules/sources'
+import db from '../../queries'
 
 const INTERVAL = 20000
 
@@ -27,26 +25,24 @@ export default ({ config }) => {
 
 let createAllNews = (req, res) => {
   let time = Math.floor(Date.now() / 1000)
-  console.log('All News Updated @ ', time, " | Current Interval: ", INTERVAL/1000, ' seconds')
+  console.log('All News Updated @ ', time, ' | Current Interval: ', INTERVAL / 1000, ' seconds')
   const sources = getSources()
   sources.then((apiResponses) => {
     // Flatten the array
     // From: [['source1article1', 'source1article2'], ['source2article1'], ...]
     // To: ['source1article1', 'source1article2', 'source2article1', ...]
-    
+
     // Pass the articles as parameter
     const wordCloud = processWordBank(apiResponses)
     // Respond with the processed object
-    //res.json( wordCloud )
+    // res.json( wordCloud )
     db.updateAllNews(wordCloud)
   })
 }
 
-
 // create list of words (sourceString) by pulling news data from various sources
 var getSources = () => {
-	return getNewsApi()
-
+  return getNewsApi()
 }
 // NEWS API
 // GET top 10 news article titles from News API (news sources are determined by the values of newsApiSource array)
@@ -60,35 +56,42 @@ var getNewsApi = () => {
     return rp(options)
       .then((res) => {
         let articles = res.articles
+        // let wordObj = []
+        // for (var i = 0; i < articles.length; i++){
+        //   wordObj[i].article = articles[i].title + articles[i].description
+        //   wordObj[i].url = articles[i].url
+        // }
+        // console.log(wordObj[1].article, wordObj[1].url)
+        console.log(articles[1].url)
+
         let articleWords = _.map(articles, 'title') + _.map(articles, 'description')
         // grab the url of each article. each url needs to be associated with a word
-        let articleUrls = _.map(articles, 'url')
-        console.log(articleWords[1], articleUrls[1])
+        // let articleUrls = _.map(articles, 'url')
         // *TODO* figure out how best to map words and urls
         // The promise is fulfilled with the articleWords
         return articleWords
       })
       .catch((err) => {
-        console.log('Warning: NewsApi request for [', source.name, '] failed...')
+        if (err) {
+          console.log('Warning: NewsApi request for [', source.name, '] failed...')
+        }
       })
   })
   // Return the promise that is fulfilled with all request values
   return Promise.all(apiCalls)
 }
 
-// *TODO* 
-// takes a string of words and array of urls, then 
-var matchUrls = (words, urls) => {
-  let article = {}
-
-}
-
+// *TODO*
+// takes a string of words and array of urls, then
+// var matchUrls = (words, urls) => {
+//   let article = {}
+// }
 
 // clean & analyse word bank for patterns/trends
 var processWordBank = (apiResponses) => {
   let wordBank = [].concat.apply([], apiResponses)
   wordBank = wordBank.join() // combine all article titles into one array element
-	wordBank = refineSource(wordBank) // word cleanup
+  wordBank = refineSource(wordBank) // word cleanup
   wordBank = combineCommon(wordBank) // combine all words that appear more than once ex: "white house", "bernie sanders"
   wordBank = getWordFreq(wordBank) // get frequency of each word
   wordBank = sortToObject(wordBank[0], wordBank[1]) // convert word array and count array to object
@@ -98,24 +101,24 @@ var processWordBank = (apiResponses) => {
 }
 
 var normalize = (arr) => {
-  let ratio = arr[0].size / 250, l = arr.length
+  let ratio = arr[0].size / 250
+  let l = arr.length
   for (var i = 0; i < l; i++) {
     arr[i].size = Math.round(arr[i].size / ratio)
   }
   return arr
 }
 
-
 // clean up list of words (remove unwanted chars, toLowerCase, remove undefined) and return it in array form
 var refineSource = (str) => {
-	var arr = str.replace(/–|"|,|!|\?|\u2022|-|—|:|' | '|\/r\/|  /g, ' ') // remove most special chars, replace with " "
-		.replace(/\.|…|'s|[\u2018\u2019\u201A\u201B\u2032\u2035]|\|/g, '') // remove specific chars
-    .split(' ') // convert to array
-    .map((x) => x.toLowerCase()) // convert all elements to lowercase
+  var arr = str.replace(/–|"|,|!|\?|\u2022|-|—|:|' | '|\/r\/| {2}/g, ' ') // remove most special chars, replace with " "
+  .replace(/\.|…|'s|[\u2018\u2019\u201A\u201B\u2032\u2035]|\|/g, '') // remove specific chars
+  .split(' ') // convert to array
+  .map((x) => x.toLowerCase()) // convert all elements to lowercase
   // remove all unwanted words using the filteredWord array
   for (var i = 0; i < arr.length; i++) {
     for (var j = 0; j < filteredWord.length; j++) {
-      if (arr[i] == filteredWord[j]) {
+      if (arr[i] === filteredWord[j]) {
         arr[i] = ''
       }
     }
@@ -123,7 +126,6 @@ var refineSource = (str) => {
   // remove any items that are just empty spaces
   arr.clean('') // remove any elements that are null
   return arr
-	
 }
 
 // find array elements that appear together more than once, if so, combine them into single element
@@ -131,13 +133,15 @@ var combineCommon = (arr) => {
   var dictionary = {}
   for (var a = 0; a < arr.length; a++) {
     var A = arr[a]
-    if (dictionary[A] == undefined) {
+    if (dictionary[A] === undefined) {
       dictionary[A] = []
     }
     dictionary[A].push(arr[a + 1])
   }
-  var res = [];
-  arr.clean('')
+  var res = []
+  arr = clean(arr, '')
+  arr = clean(arr, undefined)
+
   arr.clean(undefined)
   for (var index = 0; index < arr.length; index++) {
     var element = arr[index]
@@ -145,13 +149,13 @@ var combineCommon = (arr) => {
     if (typeof dictionary[element] !== 'undefined' && dictionary[element].length > 1) {
       if (dictionary[element]
         .some((a) => {
-          return a != dictionary[element][0]
-        }) == false) {
+          return a !== dictionary[element][0]
+        }) === false) {
         pass = true
       }
     }
     if (pass) {
-      res.push(arr[index] + " " + dictionary[element][0])
+      res.push(arr[index] + ' ' + dictionary[element][0])
       index++
     } else {
       res.push(arr[index])
@@ -160,52 +164,53 @@ var combineCommon = (arr) => {
   return res
 }
 
-
 var getWordFreq = (arr) => {
-    var a = [], b = [], prev;
+  var a = []
+  var b = []
+  var prev
 
-    arr.sort()
-    for ( var i = 0; i < arr.length; i++ ) {
-        if ( arr[i] !== prev ) {
-            a.push(arr[i])
-            b.push(1)
-        } else {
-            b[b.length-1]++
-        }
-        prev = arr[i]
+  arr.sort()
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] !== prev) {
+      a.push(arr[i])
+      b.push(1)
+    } else {
+      b[b.length - 1]++
     }
+    prev = arr[i]
+  }
 
-    return [a, b]
+  return [a, b]
 }
 
 var sortToObject = (words, count) => {
   var obj = []
-  for (var i = 0; i < words.length; i++){
+  for (var i = 0; i < words.length; i++) {
     var element = {}
     element.text = words[i]
-    count[i] = count[i]*2
+    count[i] = count[i] * 2
     element.size = count[i]
     obj.push(element)
   }
-  obj.sort((a,b) =>{
-      return b.size - a.size
-      }
+  obj.sort((a, b) => {
+    return b.size - a.size
+  }
   )
   // assign each pair an id (1 - ?)
-  for (var i = 0; i < obj.length; i++){
+  for (i = 0; i < obj.length; i++) {
     obj[i].id = i + 1
   }
   return obj
 }
 
 // utility
-Array.prototype.clean = function(deleteValue) {
-  for (var i = 0; i < this.length; i++) {
-    if (this[i] == deleteValue) {         
-      this.splice(i, 1)
+var clean = (arr, deleteValue) => {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === deleteValue) {
+      arr.splice(i, 1)
       i--
     }
   }
-  return this
+  return arr
 }
 setInterval(createAllNews, INTERVAL)
--- src/api/news/modules/sources.js
+++ src/api/news/modules/sources.js
@@ -1,280 +1,289 @@
-const newsSection	    = ["business", "entertainment", "all", "politics", "sports", "technology"]
+// const newsSection = ['business', 'entertainment', 'all', 'politics', 'sports', 'technology']
 
 export const newsApiSource = [
   {
-    "name": "abc-news-au",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'abc-news-au',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
 
   },
   {
-    "name": "ars-technica",
-    "sector": ["technology"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'ars-technica',
+    'sector': ['technology'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "associated-press",
-    "sector": ["general", "politics", "business"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'associated-press',
+    'sector': ['general', 'politics', 'business'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "bbc-news",
-    "sector": ["general", "politics", "business", "sports"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'bbc-news',
+    'sector': ['general', 'politics', 'business', 'sports'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "bbc-sport",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'bbc-sport',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "bloomberg",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'bloomberg',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "business-insider",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'business-insider',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "cnbc",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'cnbc',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "engadget",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'engadget',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "entertainment-weekly",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'entertainment-weekly',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "espn",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'espn',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "financial-times",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'financial-times',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "fox-sports",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'fox-sports',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "google-news",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'google-news',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "hacker-news",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'hacker-news',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "ign",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'ign',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "independent",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'independent',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "mashable",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'mashable',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "mtv-news",
-    "sector": ["entertainment"],
-    "rank": 3,
-    "sort": "latest"
+    'name': 'mtv-news',
+    'sector': ['entertainment'],
+    'rank': 3,
+    'sort': 'latest'
   },
   {
-    "name": "national-geographic",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'national-geographic',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "new-scientist",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'new-scientist',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "newsweek",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'newsweek',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "reddit-r-all",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'reddit-r-all',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "reuters",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'reuters',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "techcrunch",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'techcrunch',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "the-economist",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'the-economist',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "the-guardian-uk",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'the-guardian-uk',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "the-huffington-post",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'the-huffington-post',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "the-new-york-times",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'the-new-york-times',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "the-next-web",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "latest"
+    'name': 'the-next-web',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'latest'
   },
   {
-    "name": "the-verge",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'the-verge',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "the-wall-street-journal",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'the-wall-street-journal',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "the-washington-post",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'the-washington-post',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "time",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'time',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   },
   {
-    "name": "usa-today",
-    "sector": ["general"],
-    "rank": 4,
-    "sort": "top"
+    'name': 'usa-today',
+    'sector': ['general'],
+    'rank': 4,
+    'sort': 'top'
   }
 ]
 export const filteredWord = [
-  "(ap)",
-  "have",
-  "no",
-  "yes",
-  "can",
-  "ign",
-  "which",
-  "some",
-  "were",
-  "he",
-  "she",
-  "about",
-  "his",
-  "her",
-  "between",
-  "just",
-  "when",
-  "too",
-  "also",
-  "they",
-  "that",
-  "&",
-  "has",
-  "been",
-  "into",
-  "had",
-  "not",
-  "this",
-  "was",
-  "its",
-  "their",
-  "so",
-  "after",
-  "with",
-  "over",
-  "what",
-  "from",
-  "by",
-  "an",
-  "could",
-  "how",
-  "i",
-  "it",
-  "be",
-  "if",
-  "in",
-  "then",
-  "than",
-  "a",
-  "but",
-  "or",
-  "and",
-  "why",
-  "on",
-  "out",
-  "is",
-  "at",
-  "to",
-  "the",
-  "are",
-  "for",
-  "of",
-  "as"
+  '(ap)',
+  'have',
+  'no',
+  'yes',
+  'can',
+  'ign',
+  'which',
+  'some',
+  'were',
+  'he',
+  'she',
+  'about',
+  'his',
+  'her',
+  'between',
+  'just',
+  'when',
+  'too',
+  'also',
+  'they',
+  'that',
+  '&',
+  'has',
+  'been',
+  'into',
+  'had',
+  'not',
+  'this',
+  'was',
+  'its',
+  'their',
+  'so',
+  'after',
+  'with',
+  'over',
+  'what',
+  'from',
+  'by',
+  'an',
+  'could',
+  'how',
+  'i',
+  'it',
+  'be',
+  'if',
+  'in',
+  'then',
+  'than',
+  'a',
+  'but',
+  'or',
+  'and',
+  'why',
+  'on',
+  'out',
+  'is',
+  'at',
+  'to',
+  'the',
+  'are',
+  'for',
+  'of',
+  'as',
+  'you',
+  'your',
+  'where',
+  'while',
+  'these',
+  'during',
+  'fox',
+  'do',
+  'there'
 ]
