9ce8766d96b190da11ac98fc4e04de57bba3b737
wrobbinz <wrobbinz@gmail.com>
2017-10-29 00:58:34 -0700

    strip out more bodyparser

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,11 +1,9 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import db from '../models/'
 
 
 const app = express()
 const router = express.Router()
-app.use(bodyParser.json())
 
 router.get('/', (req, res) => {
   db.Words.find().then((words) => {
@@ -14,6 +12,22 @@ router.get('/', (req, res) => {
       message: 'Retrieved all news successfully',
       count: words.length,
       data: words,
+    })
+  }).catch((err) => {
+    res.status(400).send({
+      status: 400,
+      message: err.message,
+    })
+  })
+})
+
+router.get('/:id', (req, res) => {
+  db.Words.findById(req.params.id).then((word) => {
+    res.status(200).send({
+      status: 200,
+      message: `Retrieved news by ID: ${req.params.id}`,
+      count: 1,
+      data: word,
     })
   }).catch((err) => {
     res.status(400).send({
