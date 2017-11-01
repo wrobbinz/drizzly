32599bbc559a045248d5cd25d19257eba4cabdb4
Will Robbins <will@sighten.io>
2017-10-31 17:41:10 -0700

    add NODE_ENV to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo $'\n'"🌧  Drizzy Deploy 🌧"$'\n'"============================"$'\n'
 
--read -p "Enter NewsAPI Auth Token: " val
+-read -p 'Enter NewsAPI Auth Token: ' val
 export NEWSAPI_PASS=$val
-
+export NODE_ENV='production'
 npm install
 npm start
