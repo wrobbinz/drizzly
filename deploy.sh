2afaf9b66b911b17937ffa8a8651616c99535ad6
wrobbinz <wrobbinz@gmail.com>
2017-11-02 19:10:42 -0700

    add port

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ echo $'\n'"🌧  Drizzy Deploy 🌧"$'\n'"============================"$'\n'
 -read -p 'Enter NewsAPI Auth Token: ' val
 export NEWSAPI_PASS=$val
 export NODE_ENV='production'
+export PORT='6060'
 npm install
 npm start
